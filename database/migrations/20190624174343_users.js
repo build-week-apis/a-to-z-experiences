e93db363a63a5d77e67b416953226f68c1cbdb2b
Leslie Thompson <lcrthompson@gmail.com>
2019-06-24 18:27:37 -0400

    deployed to heroku

---
+++ database/migrations/20190624174343_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("user", user => {
+    user.increments();
+    user
+      .string("email", 255)
+      .notNullable()
+      .unique();
+    user.string("password", 255).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("user");
+};
