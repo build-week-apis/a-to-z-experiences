d886579f78256e695a6115b45f6e9726479b833b
Leslie Thompson <lcrthompson@gmail.com>
2019-06-24 17:23:25 -0400

    server running

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const server = express();
 
 const configureRoutes = require("../config/routes.js");
 
@@ -9,5 +10,10 @@ server.use(cors());
 server.use(express.json());
 
 configureRoutes(server);
+
+// sanity check
+server.get("/", (req, res) => {
+  res.send(`<h2> I'm runninnnnnnnnnnn</h2>`);
+});
 
 module.exports = server;
