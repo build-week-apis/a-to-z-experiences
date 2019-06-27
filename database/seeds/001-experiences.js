6da6d75ad1697dabaeabe5fa223253fc8d20693d
Leslie Thompson <lcrthompson@gmail.com>
2019-06-27 10:07:33 -0400

    playing with faker

--- database/seeds/001-experiences.js
+++ database/seeds/001-experiences.js
@@ -1,43 +1,60 @@
 const faker = require("faker");
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("experiences")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("experiences").insert([
-        {
-          user_id: 1,
-          title: "Arts & crafts",
-          date: "9/2/19",
-          location: "Brooklyn, NY",
-          price: "$20",
-          description: "Art lessons for all ages"
-        },
-        {
-          user_id: 2,
-          title: "Swimming",
-          date: "7/28/19",
-          location: "San Francisco, CA",
-          price: "$30",
-          description: "Swimming lessons for the family"
-        },
-        {
-          user_id: 1,
-          title: "Fishing",
-          date: "11/12/19",
-          location: "Princeton, NJ",
-          price: "$15",
-          description: "Family friendly fishing trip"
-        },
-        {
-          user_id: 1,
-          title: "Knitting",
-          date: "7/28/19",
-          location: "San Francisco, CA",
-          price: "$30",
-          description: "Knitting lessons for the family"
-        }
-      ]);
-    });
+
+const createFakeExperiences = () => ({
+  title: faker.lorem.words(),
+  date: faker.date.future(),
+  location: faker.address.city(),
+  price: faker.random.number(),
+  description: faker.lorem.sentences()
+});
+exports.seed = async function(knex, Promise) {
+  //experiences
+  const fakeExperiences = [];
+  const desiredFakeExperiences = 15;
+  for (let i = 0; i < desiredFakeExperiences; i++) {
+    fakeExperiences.push(createFakeExperiences());
+  }
+  await knex("experiences").insert(fakeExperiences);
 };
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex("experiences")
+//     .truncate()
+//     .then(function() {
+//       // Inserts seed entries
+//       return knex("experiences").insert([
+//         {
+//           user_id: 1,
+//           title: "Arts & crafts",
+//           date: "9/2/19",
+//           location: "Brooklyn, NY",
+//           price: "$20",
+//           description: "Art lessons for all ages"
+//         },
+//         {
+//           user_id: 2,
+//           title: "Swimming",
+//           date: "7/28/19",
+//           location: "San Francisco, CA",
+//           price: "$30",
+//           description: "Swimming lessons for the family"
+//         },
+//         {
+//           user_id: 1,
+//           title: "Fishing",
+//           date: "11/12/19",
+//           location: "Princeton, NJ",
+//           price: "$15",
+//           description: "Family friendly fishing trip"
+//         },
+//         {
+//           user_id: 1,
+//           title: "Knitting",
+//           date: "7/28/19",
+//           location: "San Francisco, CA",
+//           price: "$30",
+//           description: "Knitting lessons for the family"
+//         }
+//       ]);
+//     });
+// };
