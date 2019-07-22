6d8167270683ba5443e49dd3188f5183db42e5c5
Adam Hinckley <adamhinckley@mac.com>
2019-07-22 17:00:30 -0600

    adds production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,50 @@
 // Update with your config settings.
 module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: { filename: "./database/auth.db3" },
-    useNullAsDefault: true,
+    development: {
+        client: "sqlite3",
+        connection: { filename: "./database/auth.db3" },
+        useNullAsDefault: true,
 
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            }
+        },
+        migrations: {
+            directory: "./database/migrations"
+        },
+        seeds: { directory: "./database/seeds" }
     },
-    migrations: {
-      directory: "./database/migrations"
+    testing: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/test.db3"
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            }
+        },
+        migrations: {
+            directory: "./database/migrations"
+        },
+        seeds: {
+            directory: "./database/seeds"
+        }
     },
-    seeds: { directory: "./database/seeds" }
-  },
-  testing: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/test.db3"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
-    migrations: {
-      directory: "./database/migrations"
-    },
-    seeds: {
-      directory: "./database/seeds"
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: "./database/migrations"
+        },
+        seeds: {
+            directory: "./database/seeds/"
+        }
     }
-  }
 };
